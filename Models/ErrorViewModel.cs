75e1bc1c6c5bb5cff73ce8500f0d46dfec1d5a63
jrgallentine <76459959+jrgallentine@users.noreply.github.com>
2021-03-02 14:48:09 -0500

    First commit with ignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BreweryAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
